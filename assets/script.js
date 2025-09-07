50516bf69af414a7f3695efd2ace6c47d4901ce5
sineadezita <s.ezita@icloud.com>
2025-09-07 14:41:57 +0100

    Add core JS variables and DOM references

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+let balance = 10000;
+let portfolio = { LVMH: 0, Prada: 0, Burberry: 0 };
+const stocks = { LVMH: 812.5, Prada: 116.2, Burberry: 235.4 };
+
+const gameSection = document.getELementById("game");
+const startBtn = document.getElementById("start-btn");
