86b494f68bbaff43289cd0e27878a18a592f2551
sineadezita <s.ezita@icloud.com>
2025-09-17 20:16:31 +0100

    Calculate each stock's current value, profit/loss and update total portfolio value

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,10 @@ let portfolio = { LVMH: 0, Prada: 0, Burberry: 0 };
 const stocks = { LVMH: 812.5, Prada: 116.2, Burberry: 235.4 };
 
 let liveInterval = null; //for live updates
+let startingBalance = 10000;
+let currentLevel = 1;
 
+// Dom Elemants
 const gameSection = document.getElementById("game");
 const startBtn = document.getElementById("start-btn");
 const liveToggle = document.getElementById("live-toggle");
@@ -36,17 +39,39 @@ function renderStocks() {
 
 // Update portfolio
 function updatePortfolio() {
+    //Update visible cash balance
     document.getElementById("balance").textContent = balance.toFixed(2);
+
     const holdingsList = document.getElementById("holdings");
     holdingsList.innerHTML = "";
+    let totalValue = balance;
+
     for (let stock in portfolio) {
-        if (portfolio[stock] > 0) { //only shows stocks you own
+        if (portfolio[stock].shares > 0) {
+        let currentValue = portfolio[stock].shares * stocks[stock];
+        let costBasis = portfolio[stock].shares * portfolio[stock].avgPrics;
+        let profitLoss = currentValue - costBasis;
+        totalValue += currentValue;
+
         let li = document.createElement("li");
+        let stockValue = portfolio[stock] * stocks[stock];
+        portfolioValue += stockValue; 
+
         li.innerHTML = `${stock}: ${portfolio[stock]} shares
         <span class="value">€${(portfolio[stock] * stocks[stock]).toFixed(2)}</span>`;
         holdingsList.appendChild(li);
         }
     }
+
+    //Add totals
+    if (portfolioValue > 0) {
+        let totalLi = document.createElement("li");
+        totalLi.innerHTML = `<strong>Total Portfolio</strong>
+            <span class="value">€${portfolioValue.toFixed(2)}</span>`;
+        holdingsList.appendChild(totalLi);
+    }
+
+
 }
 
 // Buy Stock
